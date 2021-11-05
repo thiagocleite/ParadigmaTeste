01bd60a1594499f2cba8340e56766bc4f6fc1af9
thiagocleite <thiagocleite@live.com>
2021-11-05 13:09:00 -0300

    Add files via upload

---
+++ Paradigma/OrganizarArvore.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Paradigma
+{
+    public static class OrganizarArvore
+    {
+        public static int[] Organizar(int[] arvore)
+        {
+            int aux = -1;
+            int pos = 0;
+            int raiz = 0;
+            //Extrai Raiz como base
+            for (int i = 0; i < arvore.Length; i++)
+            {
+                if (arvore[i] > aux)
+                {
+                    aux = arvore[i];
+                    pos = i;
+                }
+            }
+
+            raiz = aux;
+
+            //Ordena galho esquerdo
+            for (int i = 0; i < pos - 1; i++)
+            {
+                for (int j = 0; j < pos - 1; j++)
+                {
+                    if (arvore[j] > arvore[j + 1])
+                    {
+                        aux = arvore[j];
+                        arvore[j] = arvore[j + 1];
+                        arvore[j + 1] = aux;
+                    }
+                }
+            }
+            //Ordena galho direito
+            for (int i = pos + 1; i < arvore.Length - 1; i++)
+            {
+                for (int j = pos + 1; j < arvore.Length - 1; j++)
+                {
+
+                    if (arvore[j] < arvore[j + 1])
+                    {
+                        aux = arvore[j];
+                        arvore[j] = arvore[j + 1];
+                        arvore[j + 1] = aux;
+                    }
+                }
+            }
+            //retorna arvore ordenada
+            return arvore;
+        }
+    }
+}
